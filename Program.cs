39cc90927e29929373328a1deac80ec9fbe0993d
Jean Carlos <jcpds14@gmail.com>
2022-09-23 05:23:38 -0300

    apply UTF8 encoding

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
 ﻿using DesaafioEstacionamentoJean.Models;
+
+//apply encoding for UTF8 to display accentuation
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal initialPrice = 0;
+decimal hoursPrice = 0;
